9999409b140985f2815faa6bb62a5a74df8463e4
timm-dev <timothy.morrison@lowe-and-fletcher.co.uk>
2022-11-26 15:09:11 +0000

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,87 @@
+import User from "../models/user"
+import { hashPassword, comparePassword } from "../helpers/auth";
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+  try {
+    // validation
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: "is required",
+      });
+    }
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "password is required and should be  6 charactors long",
+      });
+    }
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is taken",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    try {
+      const user = await new User({
+        name,
+        email,
+        password : hashedPassword,
+      }).save();
+
+      const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+  
+      
+      console.log(user);
+      
+      const { password, ...rest } = user._doc;
+      return res.json({
+        token,
+        user: rest,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // check email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({
+        error: "No user found",
+      });
+    }
+    //check password
+    const match = await comparePassword(req.body.password, user.password)
+    if(!match) {
+      return res.json({
+        error: "Wrong password",
+      })
+    }
+    // create signed token
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+  });
+
+  const {password, ...rest} = user._doc;
+  res.json({
+    token,
+    user: rest,
+  })
+
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router();
+
+import { login, register } from "../controllers/auth";
+
+router.post("/register", register);
+router.post("/login", login)
+
+module.exports = router;
